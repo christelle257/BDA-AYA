c00b3698d330fd25bd8d658f2d307c49ffcf7993
josiane257 <51795239+josiane257@users.noreply.github.com>
2020-10-23 23:15:32 +0000

    add etudiant

---
+++ classes/etudiant.py
@@ -0,0 +1,14 @@
+import fonctions_db
+import classes
+
+class Etudiant(object):
+	def __init__(self, matricule, cursor=None, stop=1, etudiant=None,):
+		if cursor is None:
+			conn, cursor = fonctions_db.connect_db()
+		if etudiant == None:
+			etudiant = fonctions_db.execute_query(conn if conn else None, cursor,
+			                                    f"""SELECT * FROM etudiant WHERE Matricule = '{matricule}' """,
+			                                    is_result=True)
+		self.Matricule, self.Nom, self.Prenoms, self.Sexe, self.classe = etudiant[0], etudiant[1], etudiant[2], etudiant[3], classes.classe.Classe(code_cl=etudiant[4])
+		if stop == 1:
+			fonctions_db.close_db(conn)
