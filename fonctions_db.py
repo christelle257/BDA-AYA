90c3a9180051d7e0f1ead83159ca654473b3a7de
josiane257 <51795239+josiane257@users.noreply.github.com>
2020-10-23 01:18:23 +0000

    Update fonctions_db.py

--- fonctions_db.py
+++ fonctions_db.py
@@ -21,3 +21,29 @@ def close_db(conn):
 	"""Fonction permettant de fermer la base de donnée"""
 	conn.commit()
 	conn.close()
+
+
+def execute_query(conn: None, cursor: None, query: str, values=None, is_excute_many=False, is_result=False,
+                  is_result_many=False, close=True):
+	myresult = None
+	if not (conn and cursor):
+		conn, cursor = connect_db()
+	if not values:
+		cursor.execute(query)
+	elif not is_excute_many:
+		cursor.excute(query, values)
+	else:
+		cursor.executemany(query, values)
+	if is_result:
+		if not is_result_many:
+			myresult = cursor.fetchone()
+		else:
+			myresult = cursor.fetchall()
+	if close:
+		close_db(conn)
+	else:
+		conn.commit()
+	if myresult:
+		return myresult
+
+
