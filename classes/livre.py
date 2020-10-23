8f473419107a9c0c99ad854035ddca21b2f19e6c
josiane257 <51795239+josiane257@users.noreply.github.com>
2020-10-23 23:22:04 +0000

    update livre.py

---
+++ classes/livre.py
@@ -0,0 +1,12 @@
+import fonctions_db
+
+
+class Livre(object):
+	def __init__(self, code_liv, cursor=None, stop=1, livre=None):
+		if cursor == None:
+			conn, cursor = fonctions_db.connect_db()
+		if livre == None:
+			livre = fonctions_db.execute_query(conn if conn else None, cursor,
+			                                    f"""SELECT * FROM livre WHERE Code_Liv = '{code_liv}' """,
+			                                    is_result=True)
+		self.Code_Liv, self.Titre, self.Auteur, self.Genre, self.Prix = livre[0], livre[1], livre[2], livre[3], livre[4]
