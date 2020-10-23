221c0dc9d27c3b3e7d7b7d68172fe41a6ebdf6f2
josiane257 <51795239+josiane257@users.noreply.github.com>
2020-10-23 22:59:41 +0000

    generate

---
+++ classes/classe.py
@@ -0,0 +1,14 @@
+import fonctions_db
+
+class Classe(object):
+	def __init__(self, code_cl, cursor=None, stop=1, classe=None, eleve=None):
+		if cursor == None:
+			conn, cursor = fonctions_db.connect_db()
+		if classe == None:
+			classe = fonctions_db.execute_query(conn if conn else None, cursor, f"""SELECT * FROM classe WHERE Code_Cl = '{code_cl}' """,is_result=True)
+		self.Code_Cl, self.Intitule, self.Effectif = classe[0], classe[1], classe[2]
+
+		if eleve == None:
+			self.charge_eleves()
+		if stop == 1:
+			fonctions_db.close_db(conn)
