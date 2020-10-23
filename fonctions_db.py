3fa477f48a7a78129a71fd29ef38af62884ff255
josiane257 <51795239+josiane257@users.noreply.github.com>
2020-10-23 22:47:46 +0000

    Création base de donnée

--- fonctions_db.py
+++ fonctions_db.py
@@ -58,3 +58,59 @@ def execute_query(conn: None, cursor: None, query: str, values=None, is_excute_m
 		conn.commit()
 	if myresult:
 		return myresult
+
+
+def create_db(conn, cursor):
+	execute_query(conn, cursor, """
+	create table if not exists classe
+		(
+		    Code_Cl  varchar(6)  not null primary key ,
+		    Intitule varchar(30) not null,
+		    Effectif int         not null,
+		    constraint classe_Code_Cl_uindex
+		        unique (Code_Cl)
+		);
+	""")
+	execute_query(conn, cursor, """
+	create table if not exists livre
+		(
+		    Code_Liv varchar(15) not null,
+		    Titre    varchar(40) not null,
+		    Auteur   varchar(30) not null,
+		    Genre    varchar(20) not null,
+		    Prix     int         not null,
+		    constraint LIVRE_Code_Liv_uindex
+		        unique (Code_Liv)
+		);
+	""")
+	execute_query(conn, cursor, """
+		create table if not exists etudiant
+		(
+		    Matricule varchar(6)  not null primary key ,
+		    Nom       varchar(20) not null,
+		    Prenoms   varchar(50) not null,
+		    Sexe      int(1)      not null,
+		    Code_Cl   varchar(6)  not null,
+		    constraint ETUDIANT_Matricule_uindex
+		        unique (Matricule)
+		);
+		
+		create index ETUDIANT_classe_Code_Cl_fk
+		    on etudiant (Code_Cl);
+		""")
+	execute_query(conn, cursor, """
+		create table if not exists emprunt
+		(
+		    Matricule varchar(6)  not null,
+		    Code_Liv  varchar(15) not null,
+		    Sortie    datetime    null,
+		    Retour    datetime    null
+		);
+		
+		create index emprunt_etudiant_Matricule_fk
+		    on emprunt (Matricule);
+		
+		create index emprunt_livre_Code_Liv_fk
+		    on emprunt (Code_Liv);
+		""")
+	close_db()
